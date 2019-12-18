3a34a656689d0c61e5b83f12893b15660f406628
Sadettin Kepenek <Sadettin.Kepenek@hotmail.com>
2019-12-18 23:31:55 +0300

    Modify ColumnType

--- T-API.SqlGenerator/ColumnType.cs
+++ T-API.SqlGenerator/ColumnType.cs
@@ -17,9 +17,29 @@
         #region Approximate Numerics
         public static readonly string FLOAT = "float";
         public static readonly string REAL = "real";
+        #endregion
 
+        #region date and datetime
+        public static readonly string DATE = "date";
+        public static readonly string DATETIME = "datetime";
+        public static readonly string DATETIME2 = "datetime2";
+        public static readonly string SMALLDATETIME = "smalldatetime";
+        public static readonly string TIME = "time";
+        #endregion
 
+        #region strings
+        public static readonly string CHAR = "char";
+        public static readonly string VARCHAR = "varchar";
+        public static readonly string TEXT = "text";
+        public static readonly string NCHAR = "nchar";
+        public static readonly string NVARCHAR = "nvarchar";
+        public static readonly string NTEXT = "ntext";
+        public static readonly string IMAGE = "image";
+        #endregion
 
+        #region other
+        public static readonly string UNIQUEIDENTIFIER = "uniqueidentifier";
+        public static readonly string XML = "xml";
         #endregion
 
     }

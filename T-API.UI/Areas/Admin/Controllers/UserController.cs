80cb10e17b824702e09f3200ee2f06ba7b59dfa6
BERKAY YALCIN <byalcin16@posta.pau.edu.tr>
2019-12-21 02:00:07 +0300

    Add Roles

--- T-API.UI/Areas/Admin/Controllers/UserController.cs
+++ T-API.UI/Areas/Admin/Controllers/UserController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using T_API.UI.Areas.Admin.Models.User;
 
 namespace T_API.UI.Areas.Admin.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class UserController : Controller
     {
         private IMapper _mapper;

b4fffca4dee31d0ecd38c48e5e591025b84c9e68
BERKAY YALCIN <byalcin16@posta.pau.edu.tr>
2019-12-21 01:59:46 +0300

    Add Admin UserController

---
+++ T-API.UI/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using T_API.UI.Areas.Admin.Models.User;
+
+namespace T_API.UI.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        private IMapper _mapper;
+
+        public UserController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+            // TODO User Listeleme
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(CreateUserViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+            
+
+            return View();
+        }
+
+
+    }
+}

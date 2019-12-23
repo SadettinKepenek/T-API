00d8e8ac80997bbc7937ed07bb3c7f4916463525
BERKAY YALCIN <byalcin16@posta.pau.edu.tr>
2019-12-23 18:06:38 +0300

    Add table Dtos

---
+++ T-API.Core/DTO/Table/ListTableDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+using T_API.Core.DTO.Column;
+using T_API.Core.DTO.ForeignKey;
+using T_API.Core.DTO.Index;
+using T_API.Core.DTO.Key;
+
+namespace T_API.Core.DTO.Table
+{
+    public class ListTableDto
+    {
+        public string TableName { get; set; }
+        public ObservableCollection<ListForeignKeyDto> ForeignKeys { get; set; }
+        public ObservableCollection<ListIndexDto> Indices { get; set; }
+        public ObservableCollection<ListKeyDto> Keys { get; set; }
+        public ObservableCollection<ListColumnDto> Columns { get; set; }
+    }
+}

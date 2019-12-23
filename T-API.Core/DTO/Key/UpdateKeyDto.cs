415ee60e31d2fd3faac394a060543588ab9c1b01
BERKAY YALCIN <byalcin16@posta.pau.edu.tr>
2019-12-23 18:04:59 +0300

    Add Key Dtos

---
+++ T-API.Core/DTO/Key/UpdateKeyDto.cs
@@ -0,0 +1,11 @@
+﻿namespace T_API.Core.DTO.Key
+{
+    public class UpdateKeyDto
+    {
+        public string KeyName { get; set; }
+        public string KeyColumn { get; set; }
+        public bool IsPrimary { get; set; }
+
+        public string TableName { get; set; }
+    }
+}

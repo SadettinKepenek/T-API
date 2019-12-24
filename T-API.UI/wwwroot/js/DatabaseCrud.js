e14fa44c5d6b7cdfbe863b531ab1e55aaace1945
BERKAY YALCIN <byalcin16@posta.pau.edu.tr>
2019-12-25 01:12:23 +0300

    Get Tables Done

--- T-API.UI/wwwroot/js/DatabaseCrud.js
+++ T-API.UI/wwwroot/js/DatabaseCrud.js
@@ -3,62 +3,86 @@ var getDataTypes = function getDataTypes() {
 
 };
 
-var addTableContainer = function addTable() {
-    var tabTables = $('#v-pills-tab-tables');
-    var tabTablesString = '<a class="nav-link ';
-    if (window.tableCount === 0) {
-        tabTablesString += ' active"';
-    } else {
-        tabTablesString += '"';
-    }
-    tabTablesString += ' id="v-pills-table_' + window.tableCount + '-tab" ' +
-        'data-toggle="pill"' +
-        'href="#v-pills-' + window.tableCount + '"' +
-        'role="tab" aria-controls="v-pills-home" aria-selected="true">Test</a>';
+var addTableContainer = function addTable(object) {
 
+    var table = JSON.parse(object);
+    var tabTables = $('#v-pills-tab-tables');
     var tableContent = $('#v-pills-tabContent-tables');
 
-    var tableContentString = '<div class="tab-pane fade ';
-    if (window.tableCount === 0) {
-        tableContentString += ' show active"';
-    } else {
-        tableContentString += '"';
-    }
+
+    table.forEach(function (table) {
+        var tabTablesString = '<a class="nav-link ';
+        if (window.tableCount === 0) {
+            tabTablesString += ' active"';
+        } else {
+            tabTablesString += '"';
+        }
+        tabTablesString += ' id="v-pills-table_' + window.tableCount + '-tab" ' +
+            'data-toggle="pill"' +
+            'href="#v-pills-' + window.tableCount + '"' +
+            'role="tab" aria-controls="v-pills-home" aria-selected="true">' + table.tableName + '</a>';
+
+        var tableContentString = '<div class="tab-pane fade ';
+        if (window.tableCount === 0) {
+            tableContentString += ' show active"';
+        } else {
+            tableContentString += '"';
+        }
+
+
+        tableContentString += 'id="v-pills-' + window.tableCount + '" role="tabpanel" ' +
+            'aria-labelledby="v-pills-home-tab">';
+        tableContentString += '<div class="container">';
+
+        tableContentString += '<div class="row">';
+        tableContentString += '<div class="col-md-2">';
+        tableContentString += 'Column Name';
+        tableContentString += '</div>';
+        tableContentString += '<div class="col-md-2">';
+        tableContentString += 'Data Type';
+        tableContentString += '</div>';
+        tableContentString += '<div class="col-md-2">';
+        tableContentString += 'Default Value';
+        tableContentString += '</div>';
+        tableContentString += '<div class="col-md-2">';
+        tableContentString += 'Is Primary';
+        tableContentString += '</div>';
+        tableContentString += '<div class="col-md-2">';
+        tableContentString += 'Is Auto Inc';
+        tableContentString += '</div>';
+        tableContentString += '<hr/>';
+        tableContentString += '</div>';
+        tabTables.append(tabTablesString);
+
+        table.columns.forEach(function (column) {
+            tableContentString += '<hr/>';
+            tableContentString += '<div class="row">';
+            tableContentString += '<div class="col-md-2">';
+            tableContentString += column.columnName;
+            tableContentString += '</div>';
+            tableContentString += '<div class="col-md-2">';
+            tableContentString += column.dataType;
+            tableContentString += '</div>';
+            tableContentString += '<div class="col-md-2">';
+            tableContentString += column.defaultValue;
+            console.log(column.defaultValue);
+            tableContentString += '</div>';
+            tableContentString += '<div class="col-md-2">';
+            tableContentString += column.primaryKey;
+            tableContentString += '</div>';
+            tableContentString += '<div class="col-md-2">';
+            tableContentString += column.autoInc;
+            tableContentString += '</div>';
+            tableContentString += '</div>';
+        });
+        tableContentString += '</div>';
+        tableContent.append(tableContentString);
+
+        window.tableCount++;
+    });
 
 
 
-    tableContentString += 'id="v-pills-' + window.tableCount + '" role="tabpanel" ' +
-        'aria-labelledby="v-pills-home-tab">';
-    tableContentString += '<div class="container">';
-    tableContentString += '<div class="row">';
-
-    tableContentString += '<div class="col-md-2">';
-    tableContentString += 'Column Name';
-    tableContentString += '</div>';
-
-    tableContentString += '<div class="col-md-2">';
-    tableContentString += 'Data Type';
-    tableContentString += '</div>';
-
-    tableContentString += '<div class="col-md-2">';
-    tableContentString += 'Default Value';
-    tableContentString += '</div>';
-
-    tableContentString += '<div class="col-md-2">';
-    tableContentString += 'Is Primary';
-    tableContentString += '</div>';
-
-    tableContentString += '<div class="col-md-2">';
-    tableContentString += 'Is Auto Inc';
-    tableContentString += '</div>';
-
-    tableContentString += '</div>';
-    tableContentString += '</div>';
-
-    tabTables.append(tabTablesString);
-    tableContent.append(tableContentString);
-
-    window.tableCount++;
 
 };
 

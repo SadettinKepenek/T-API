d5166fb818db980143a6b12c43a4cfe7c88c3239
BERKAY YALCIN <byalcin16@posta.pau.edu.tr>
2019-12-24 03:04:55 +0300

    Modify CreateService Modify EditService

--- T-API.UI/Models/Database/EditServiceViewModel.cs
+++ T-API.UI/Models/Database/EditServiceViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
+using T_API.Core.DTO.Table;
 
 namespace T_API.UI.Models.Database
 {
@@ -26,5 +28,7 @@ namespace T_API.UI.Models.Database
         public bool IsApiSupport { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+
+        public ObservableCollection<DetailTableDto> Tables { get; set; }
     }
 }

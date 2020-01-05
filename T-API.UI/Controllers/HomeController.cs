053a69227a471f0b685f4be18e86d7799d710f39
BERKAY YALCIN <byalcin16@posta.pau.edu.tr>
2020-01-06 01:29:10 +0300

    Add Index View Model Add Documentation Action

--- T-API.UI/Controllers/HomeController.cs
+++ T-API.UI/Controllers/HomeController.cs
@@ -7,24 +7,32 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.SqlServer.Server;
+using T_API.BLL.Abstract;
 using T_API.UI.Models;
+using T_API.UI.Models.Home;
 
 namespace T_API.UI.Controllers
 {
     [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
     public class HomeController : Controller
     {
-
-        public HomeController()
+        private IPackageService _packageService;
+        public HomeController(IPackageService packageService)
         {
+            _packageService = packageService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            
+            IndexViewModel viewModel=new IndexViewModel();
+            viewModel.Packages = await _packageService.Get();
+            return View(viewModel);
+        }
+        public async Task<IActionResult> Documentation()
+        {
             return View();
         }
 
-     
+
     }
 }
---
+++ T-API.UI/Models/Home/IndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using T_API.Core.DTO.DatabasePackage;
+
+namespace T_API.UI.Models.Home
+{
+    public class IndexViewModel
+    {
+        public List<DetailDatabasePackageDto> Packages { get; set; }
+    }
+}

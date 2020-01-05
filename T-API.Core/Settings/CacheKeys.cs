3c33cec5d5f09a462abcce83a6bb8f8ab1a6823c
BERKAY YALCIN <byalcin16@posta.pau.edu.tr>
2020-01-05 23:08:16 +0300

    Add Cache Sliding Expiration Time Add Database Caching

--- T-API.Core/Settings/CacheKeys.cs
+++ T-API.Core/Settings/CacheKeys.cs
@@ -1,7 +1,10 @@
-﻿namespace T_API.Core.Settings
+﻿using System;
+
+namespace T_API.Core.Settings
 {
     public class CacheKeys
     {
         public static string DatabaseTables => "_DatabaseTables";
+        public static TimeSpan SlidingExpirationCaching => TimeSpan.FromHours(1);
     }
 }
